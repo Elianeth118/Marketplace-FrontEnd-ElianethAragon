0998ac3000bb5f9b579c9c8717580b3ea14fd08b
Elianeth118 <aragonelianeth@gmail.com>
2023-10-16 12:32:34 -0600

    Confoguracion Header-categories

--- src/app/modules/header/header/header.component.ts
+++ src/app/modules/header/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Path } from 'src/app/config';
+import { CategoriesService } from 'src/app/services/categories.service';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -7,4 +8,15 @@ import { Path } from 'src/app/config';
 })
 export class HeaderComponent {
   path:String=Path.url;
+  valorc:any;
+  constructor(private categoriesService:CategoriesService) {}
+  ngOnInit():void{
+    this.categoriesService.getdata()
+    .subscribe(resp=>{
+      this.categoriesService.getdata()
+      .subscribe((res)=>{
+        this.valorc=resp;
+      })
+    })
+  }
 }
